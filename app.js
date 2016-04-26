c08d72a2e2b97b0f3252d22c0c565a1e4b308df2
vesnavis <vesna@vislavski.com>
2016-04-25 22:43:04 -0700

    First commit

---
+++ app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+var path = require('path');
+
+
+app.get('/', function(req, res) {
+    res.sendFile('./index.html');
+});
+
+app.listen(8000);
